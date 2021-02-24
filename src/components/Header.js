6d31242d68475c13ba35137b6b909d3d7f762845
edgars61 <edgarsantana.62a@gmail.com>
2021-02-23 19:12:35 -0600

    Header Complete

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Typed from "react-typed";
+
+
+const Header = () => {
+  return (
+    <div className="header-wraper">
+      <div className="main-info">
+        <canvas></canvas>
+        <h1>Hi, my name is<br/>    Edgar Santana.
+        </h1>
+        <h2>I'm a Software Developer</h2>
+        <Typed
+          className="typed-text"
+          strings={["Web Design", "Web Development", "Facebook Ads SMM", "Google Ads"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">contact me</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,49 @@
-import React from 'react'
-import logo from "../logo.png"
-import { FontAwesomeIcon } from  "@fortawesome/react-fontawesome"
-import {faBars}  from "@fortawesome/free-solid-svg-icons"
+import React from "react";
+import logo from "../logo4.png";
 
-const Navbar = () =>{
-    return (
-  
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-        <a className="navbar-brand" href="#"><img src={logo} className="logo" alt="My Logo"></img></a>
+// REACT FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-custom navbar-expand-lg">
+      <div className="container">
+
+        <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <FontAwesomeIcon icon={faBars} style={{color:"white"}}/>
+          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
         </button>
-      
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
               <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About Me</a>
+              <a className="nav-link" href="#">about me</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Projects</a>
+              <a className="nav-link" href="#">services</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contact Me</a>
+              <a className="nav-link" href="#">how work</a>
             </li>
-           
-           
-          </ul>
-         
-        </div>
-        </div>
-      </nav>
+            <li className="nav-item">
+              <a className="nav-link" href="#">portfolio</a>
+            </li>
 
-    )
+            <li className="nav-item">
+              <a className="nav-link" href="#">contacts</a>
+            </li>
+
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
